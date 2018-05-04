071cda3791dfbd20c4d2bda966160a207bcb2adf
Andre Meyering <info@andremeyering.de>
2018-05-04 16:02:52 +0200

    Add example test

---
+++ src/test/java/refactor/CustomerTest.java
@@ -0,0 +1,14 @@
+package refactor;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class CustomerTest {
+
+    @Test
+    void myFirstTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
