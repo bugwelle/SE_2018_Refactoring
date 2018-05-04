bba07553bde3948df704efc1fcb468878debede2
Andre Meyering <info@andremeyering.de>
2018-05-04 15:05:00 +0200

    [Customer] Extract method "getFrequentRenterPoints" to Rental

--- src/Rental.java
+++ src/Rental.java
@@ -40,4 +40,12 @@ class Rental {
 
         return result;
     }
+
+    public int getFrequentRenferPoints() {
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) {
+            return 2;
+        }
+
+        return 1;
+    }
 }
