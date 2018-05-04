3a2e0a353a99a4e06096452087b2e2b278bce34a
Andre Meyering <info@andremeyering.de>
2018-05-04 15:12:50 +0200

    [Customer] Replace temp "total" with query; extract method
     - move frequent renter into its own method

--- src/Rental.java
+++ src/Rental.java
@@ -41,7 +41,7 @@ class Rental {
         return result;
     }
 
-    public int getFrequentRenferPoints() {
+    public int getFrequentRenterPoints() {
         if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) {
             return 2;
         }
